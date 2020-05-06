1c7c56715ea2ad08e280c240f39f9164fc5d0c99
Dmitry Vyukov <dvyukov@google.com>
2020-05-06 17:29:30 +0200

    fix things

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 set -eu
 
-cd "$GITHUB_WORKSPACE"
-
-golangci-lint "$@"
+golangci-lint run --print-issued-lines=0 --print-linter-name=0 --color=never "$@" | sed -E 's#(.*):([0-9]*):([0-9]*): (.*)#::error file=\1,line=\2,col=\3::\4#g'
